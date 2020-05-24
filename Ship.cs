5fbec7d59a65388641f7388b77d340575dbe34d0
Anett <aneta.kruc@gmail.com>
2020-05-24 15:02:52 +0200

    create ship class

--- Ship.cs
+++ Ship.cs
@@ -1,1 +1,30 @@
+using System.Collections.Generic;
 
+namespace battle_ship_in_the_oo_way_getonboard
+
+{
+
+    public class Ship
+    {
+    private List<Square> squares;
+    private int YCoordinates = 3;
+    private int XCoordinates = 3;
+    private int shipLength = 3;
+    private bool isVertical = false;
+
+    public Ship(List<Square> squares, int YCoordinates, int XCoordinates, int shipLength, bool isVertical)
+        {
+            this.squares = squares;
+            this.YCoordinates = YCoordinates;
+            this.XCoordinates = XCoordinates;
+            this.shipLength = shipLength;
+            this.isVertical = isVertical;
+        }
+
+    public override string ToString()
+        {
+        
+        }
+
+    }
+}
