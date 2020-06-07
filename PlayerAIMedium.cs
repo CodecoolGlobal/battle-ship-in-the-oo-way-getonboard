bec56a29cecfccf663666f4634e808d5a5403092
Adrian <flojd_xl@wp.pl>
2020-06-07 12:49:24 +0200

    Implement medium AI logic

--- PlayerAIMedium.cs
+++ PlayerAIMedium.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections.Generic;
 
 namespace battle_ship_in_the_oo_way_getonboard
 {
     public class PlayerAIMedium : Player
     {
+        public int Y {get; set;}
+        public int X {get; set;}
         public PlayerAIMedium(string name) : base(name)
         {
 
@@ -11,20 +14,74 @@ namespace battle_ship_in_the_oo_way_getonboard
 
         public int GetShipYAI()
         {
-            Random random = new Random();
-            int y = random.Next(0, 10);
-            return y;
+            return Y;
         }
 
         public int GetShipXAI()
         {
-            Random random = new Random();
-            int x = random.Next(0, 10);
-            return x;
+            return X;
+        }
+
+        public void SearchHit(Player player)
+        {
+
+            this.Y = new Random().Next(0, 10);
+
+            this.X = new Random().Next(0, 10);
+
+            foreach (Ship ship in player.shipList)
+            {
+                for (int i = 0; i < ship.GetLength(); i++)
+                {
+                    if(ship.GetSquare(i).GetIsHit() && !ship.GetIsSunk())
+                    {
+                        int squareX = ship.GetSquare(i).GetX();
+                        int squareY = ship.GetSquare(i).GetY();
+
+                        if(ship.isVertical)
+                        {
+                        Random randomX = new Random();
+                        int x = randomX.Next(squareX-2, squareX+2);
+                        this.X = x;
+
+                        Random randomY = new Random();
+                        int y = randomY.Next(squareY-3, squareY + 3);
+                        this.Y = y;
+                        }
+                        else
+                        {
+                        Random randomX = new Random();
+                        int x = randomX.Next(squareX-3, squareX + 3);
+                        this.X = x;
+
+                        Random randomY = new Random();
+                        int y = randomY.Next(squareY-2, squareY + 2);
+                        this.Y = y;
+                        }
+                    }
+                }
+            }
+            if(X>9)
+            {
+                this.X = 9;
+            }
+            else if(X<0)
+            {
+                this.X = 0;
+            }
+            if(Y>9)
+            {
+                this.Y = 9;
+            }
+            else if(Y<0)
+            {
+                this.Y = 0;
+            }
         }
 
         public string AIAttacks(Player player)
         {
+            SearchHit(player);
             return player.AttackedByAI(GetShipYAI(), GetShipXAI());
         }
         
