c25102c9e154352d08f3c553f258b1012ea624c4
Adrian <flojd_xl@wp.pl>
2020-06-07 15:06:32 +0200

    Add loop in AIEasy insted of recursion

--- PlayerAIEasy.cs
+++ PlayerAIEasy.cs
@@ -5,27 +5,42 @@ namespace battle_ship_in_the_oo_way_getonboard
 {
     public class PlayerAIEasy : Player
     {
+        public int Y {get; set;}
+        public int X {get; set;}
+
         public PlayerAIEasy(string name) : base(name)
         {
 
+        }
+
+        public void SearchHit(Player player)
+        {
+
+            this.Y = new Random().Next(0, 10);
+
+            this.X = new Random().Next(0, 10);
         }
 
         public int GetShipYAI()
         {
             Random random = new Random();
             int y = random.Next(0, 10);
-            return y;
+            return Y;
         }
 
         public int GetShipXAI()
         {
-            Random random = new Random();
-            int x = random.Next(0, 10);
-            return x;
+            return X;
         }
 
         public string AIAttacks(Player player)
         {
+            do
+            {
+            SearchHit(player);
+            }
+            while (IsSquareUsed(player.PlayerBoard.GetSquare(Y, X)));
+            
             return player.AttackedByAI(GetShipYAI(), GetShipXAI());
         }
         
